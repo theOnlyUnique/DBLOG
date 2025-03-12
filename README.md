90c59a16c061d2419f97c55360c5d55040ab1011
LQD <2548818216@qq.com>
2025-03-12 21:53:18 +0800

    feat:文章阅读栏优化

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ node version 18.18.1
 
 技术栈： React18 、 Redux toolkit 、Redux Router 、Redux Persist、Ant Design
 
-
-快速上手文档(初期看):https://ant-design.antgroup.com/docs/react/getting-started-cn
-样式库文档:https://ant-design.antgroup.com/components/overview-cn?locale=zh-CN
-TS引入参考：https://juejin.cn/post/7123111666018942990
+快速上手文档(初期看):https://ant-design.antgroup.com/docs/react/getting-started-cn<br>
+样式库文档:https://ant-design.antgroup.com/components/overview-cn?locale=zh-CN<br>
+TS引入参考：https://juejin.cn/post/7123111666018942990<br>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,5 @@
 // components/ArticleList.js
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
@@ -8,18 +8,34 @@ import { useDispatch } from 'react-redux';
 import { setArticleId } from '../store/globalSlice';
 
 const ArticleCard = ({ id,title, content }) => {
+  const [uuid] = useState(() => `article-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`);
+  // 标记是否已经展开
+  const [show,setShow]  = useState(false)
   const router = useNavigate()
   const dispatch = useDispatch()
   function getArticleDetail() {
     dispatch(setArticleId(id))
     router('/' + 'article')
   }
+  function handleLunch() {
+    const element = document.getElementById(uuid);
+    // console.log("获取ID",element,uuid);
+    if (element) {
+      element.style.maxHeight = show ? '30vh':'none';
+      setShow(!show)
+    }
+  }
   return (
     <div style={{ border: '1px solid #ddd', padding: '15px', marginBottom: '20px' ,backgroundColor:'white',overflow:'hidden'}}>
       <h2>{title}</h2>
-      <ReactMarkdown rehypePlugins={[rehypePrism]} >{content}</ReactMarkdown>
+      <div style={{ maxHeight: '30vh'}} id={uuid}>
+        <ReactMarkdown rehypePlugins={[rehypePrism]} >{content}</ReactMarkdown>
+      </div>
       <div style={{display:'flex',justifyContent: 'center'}}>
         <Button onClick={getArticleDetail}>点击阅读</Button>
+        <Button style={{ marginLeft: '10px'}} onClick={handleLunch}>
+          {show ? '收起阅读': '展开阅读'}
+        </Button>
       </div>
     </div>
   );
